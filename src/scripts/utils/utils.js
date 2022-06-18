050433dcbd1ae374106606f5857cdd53a801e126
Dylan <batista.d09@hotmail.com>
2022-06-18 15:05:56 -0400

    Added Interval param to rangeInterval function to set the interval used in the range.

--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -3,14 +3,15 @@
  *
  * @param {number} start The starting number
  * @param {number} stop The end number
+ * @param {number} interval The interval used in the range
  * @returns {number[]} The array with a sequence of numbers within the given range
  *
  */
 
-export function rangeInterval(start, stop) {
+export function rangeInterval(start, stop, interval) {
   const res = [];
 
-  for (let i = start; i <= stop; i += 100) {
+  for (let i = start; i <= stop; i += interval) {
     res.push(i);
   }
 
