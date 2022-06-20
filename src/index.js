6886065939cf924d7bb86775805fa2ef5c42f446
keshav <kdooleea@yahoo.ca>
2022-06-20 09:59:31 -0400

    changed require to import syntax

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const { playerHelperSingleton } = require("./scripts/players-helper");
-const { RadarChartManager } = require("./scripts/charts-managers/radar-chart-manager");
-const { StackedBarChartManager } = require("./scripts/charts-managers/stacked-bar-chart-manager");
-const { BarChartManager } = require("./scripts/charts-managers/bar-chart-manager");
-const { LineChartManager } = require("./scripts/charts-managers/line-chart-manager");
+"use strict";
+
+import * as BarChartManager from "./scripts/charts-managers/bar-chart-manager";
+import * as LineChartManager from "./scripts/charts-managers/line-chart-manager";
+import * as RadarChartManager from "./scripts/charts-managers/radar-chart-manager";
+import * as StackedBarChartManager from "./scripts/charts-managers/stacked-bar-chart-manager";
+import * as PlayerHelper from "./scripts/players-helper";
 
 /**
  * @file This file is the entry-point for the the code of the project.
@@ -13,10 +15,10 @@ const { LineChartManager } = require("./scripts/charts-managers/line-chart-manag
 main();
 
 async function main() {
-  await playerHelperSingleton.getSummaryData();
+  await PlayerHelper.playerHelperSingleton.getSummaryData();
 
-  const radarChartManager = new RadarChartManager();
-  const stackedBarChartManager = new StackedBarChartManager();
-  const barChartManager = new BarChartManager();
-  const lineChartManager = new LineChartManager();
+  const radarChartManager = new RadarChartManager.RadarChartManager();
+  const stackedBarChartManager = new StackedBarChartManager.StackedBarChartManager();
+  const barChartManager = new BarChartManager.BarChartManager();
+  const lineChartManager = new LineChartManager.LineChartManager();
 }
