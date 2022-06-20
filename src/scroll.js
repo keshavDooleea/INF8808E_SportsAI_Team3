4e4948104a2abee9d9101f3ce0e8bdc7ea0bbaa0
keshav <kdooleea@yahoo.ca>
2022-06-20 18:23:47 -0400

    added chart number at bottom

--- src/scroll.js
+++ src/scroll.js
@@ -1,11 +1,14 @@
 // retrieve DOM HTML elements
 const mainContainer = document.querySelector("main");
 const dots = document.querySelectorAll(".dots-container .dot");
-const dotActiveClass = "active";
-const scrollOffset = 1;
+const chartNb = document.querySelector("#chart-nb");
 
 // ids of 4 sections each holding a svg
 const sectionIds = ["#radar-chart", "#stacked-chart", "#bar-chart", "#line-chart"];
+
+// variables
+const dotActiveClass = "active";
+const scrollOffset = 1;
 
 main();
 
@@ -23,12 +26,13 @@ function main() {
 
   // add listener to main container to update window's hash and sidebar dots
   mainContainer.addEventListener("scroll", () => {
-    sectionsBoundingBoxes.forEach((box) => {
+    sectionsBoundingBoxes.forEach((box, index) => {
       const rect = box.boundingBox;
       const offsetTop = mainContainer.scrollTop;
 
       if (offsetTop > rect.top - scrollOffset && offsetTop < rect.bottom - scrollOffset) {
         box.dot.classList.add(dotActiveClass);
+        setChartNb(index + 1);
       } else {
         box.dot.classList.remove(dotActiveClass);
       }
@@ -45,10 +49,15 @@ function main() {
     dots.forEach((dot, dotIndex) => {
       if (index === dotIndex) {
         dots[index].classList.add(dotActiveClass);
+        setChartNb(index + 1);
         document.querySelector(sectionIds[index]).scrollIntoView();
       } else {
         dot.classList.remove(dotActiveClass);
       }
     });
+  }
+
+  function setChartNb(number) {
+    chartNb.textContent = number;
   }
 }
