13f1ab554dbf3ad1010c7e17adaf630790668bd1
keshav <kdooleea@yahoo.ca>
2022-06-20 13:52:34 -0400

    cleaned deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
-# abort on errors
+
 npm run build
-# navigate into the build output directory
+
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
-git commit -m 'deploying project Team 3'
+git commit -m 'deploying SportsAI project - Team 3'
 git push -f git@github.com:keshavDooleea/INF8808E_SportsAI_Team3.git master:gh-pages
+
 cd -
