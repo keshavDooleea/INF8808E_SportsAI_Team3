770128989773819694a7d8447fbcbcee243eaae7
Oliver Jean <oliver.jean@polymtl.ca>
2022-06-23 16:43:53 -0400

    started the chart

--- src/scripts/charts-managers/stacked-bar-chart-manager.js
+++ src/scripts/charts-managers/stacked-bar-chart-manager.js
@@ -7,28 +7,80 @@ import { AbstractChartManager } from './abstract-chart-manager'
  */
 export class StackedBarChartManager extends AbstractChartManager {
   preprocess () {
-    console.log('Stacked bar chart preprocess')
     this.shootingData = this.preprocessShootingData(this.playerHelperSingleton.groupedShootingData)
   }
 
-  initializeVariables () {
-    // Each one contains [Goals/Shot, PK Goals / PK attempts]
-    this.maneData = []
-    this.benzemaData = []
-    this.mbappeData = []
-  }
+  initializeVariables () {}
 
   initializeCharts () {
     this.svg = d3.select('#stacked-bar-chart-svg')
+
+    this.margin = {
+      top: 50,
+      right: 150,
+      bottom: 30,
+      left: 60,
+      leftPadding: 20
+    }
+
+    this.width = 700 - this.margin.left - this.margin.right
+    this.height = 400 - this.margin.top - this.margin.bottom
+
+    console.log(this.shootingData)
+
+    this.svg
+      .attr('width', 600)
+      .attr('height', 600)
+      .attr('padding', 10)
+
+    var groups = d3.map(this.shootingData, function (d) { return (d[0]) }).keys()
+
+    var color = d3.scaleOrdinal()
+      .domain(this.shootingData)
+      .range(['#e41a1c', '#4daf4a'])
+
+    // Add X axis
+    var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, this.width])
+      .padding([0.1])
+    this.svg.append('g')
+      .attr('transform', 'translate(' + 100 + ',' + this.height + ')')
+      .call(d3.axisBottom(x).tickSizeOuter(0))
+
+    var formatPercent = d3.format('.0%')
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, 1])
+      .range([this.height, 0])
+    this.svg.append('g')
+      .attr('transform', 'translate(' + 100 + ',' + '0' + ')')
+      .call(d3.axisLeft(y).tickFormat(formatPercent))
+
+    this.svg.append('g')
+      .selectAll('g')
+      .data(this.shootingData)
+      .enter()
+      .append('g')
+      .attr('fill', function (d) { return color(d.key) })
+      .selectAll('rect')
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function (d) { return d })
+      .enter().append('rect')
+      // .attr('x', function (d) { return x(d.Player) })
+      // .attr('y', function (d) { return y(d[1]) })
+      // .attr('height', function (d) { return y(d[0]) - y(d[1]) })
+      // .attr('width', x.bandwidth())
   }
 
   /**
-   * Calculates the number of goals, assists and shots taken during a month for a player
-   * @param {object[]} playerData the player's data parsed from CSV file
-   * @param playerName
+   * Calculates percentage of goals made and PK made for each player
+   *
    * @param shootingData
-   * @returns {object[]} The preprocessed data
-   */
+   * @returns {object[]} the preprocessed data
+   **/
+
   preprocessShootingData (shootingData) {
     console.log('preprocessShootingData')
 
@@ -39,14 +91,13 @@ export class StackedBarChartManager extends AbstractChartManager {
         PK = element.PK / element.PKatt
       }
       if (element.Player === 'Sadio Mané') {
-        this.maneData = [element.GSh, PK]
-      }
-      else if (element.Player === 'Karim Benzema') {
-        this.benzemaData = [element.GSh, PK]
-      }
-      if (element.Player === 'Kylian Mbappé') {
-        this.mbappeData = [element.GSh, PK]
+        this.maneData = [element.Player, element.GSh, PK]
+      } else if (element.Player === 'Karim Benzema') {
+        this.benzemaData = [element.Player, element.GSh, PK]
+      } else if (element.Player === 'Kylian Mbappé') {
+        this.mbappeData = [element.Player, element.GSh, PK]
       }
     })
+    return [this.maneData, this.benzemaData, this.mbappeData]
   }
 }
