0e2bc5652490841687c8ad4acd305b0dc4554559
keshav <kdooleea@yahoo.ca>
2022-06-23 09:32:48 -0400

    deploying SportsAI project - Team 3

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -36716,7 +36716,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49263" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49727" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
