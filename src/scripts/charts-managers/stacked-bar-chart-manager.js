8897f4eaf0e1eb23495b50eb7607f3ee1a197f8c
Oliver Jean <oliver.jean@polymtl.ca>
2022-06-22 23:22:07 -0400

    data is ready

--- src/scripts/charts-managers/stacked-bar-chart-manager.js
+++ src/scripts/charts-managers/stacked-bar-chart-manager.js
@@ -1,4 +1,4 @@
-import { AbstractChartManager } from "./abstract-chart-manager";
+import { AbstractChartManager } from './abstract-chart-manager'
 
 /**
  * Manager for stacked chart visualization
@@ -6,13 +6,47 @@ import { AbstractChartManager } from "./abstract-chart-manager";
  * @class StackedBarChartManager
  */
 export class StackedBarChartManager extends AbstractChartManager {
-  preprocess() {
-    console.log("Stacked bar chart preprocess");
+  preprocess () {
+    console.log('Stacked bar chart preprocess')
+    this.shootingData = this.preprocessShootingData(this.playerHelperSingleton.groupedShootingData)
   }
 
-  initializeVariables() {}
+  initializeVariables () {
+    // Each one contains [Goals/Shot, PK Goals / PK attempts]
+    this.maneData = []
+    this.benzemaData = []
+    this.mbappeData = []
+  }
 
-  initializeCharts() {
-    this.svg = d3.select("#stacked-bar-chart-svg");
+  initializeCharts () {
+    this.svg = d3.select('#stacked-bar-chart-svg')
+  }
+
+  /**
+   * Calculates the number of goals, assists and shots taken during a month for a player
+   * @param {object[]} playerData the player's data parsed from CSV file
+   * @param playerName
+   * @param shootingData
+   * @returns {object[]} The preprocessed data
+   */
+  preprocessShootingData (shootingData) {
+    console.log('preprocessShootingData')
+
+    shootingData.forEach((element, index) => {
+      var PK = 0
+      // eslint-disable-next-line eqeqeq
+      if (element.PKatt != 0) {
+        PK = element.PK / element.PKatt
+      }
+      if (element.Player === 'Sadio Mané') {
+        this.maneData = [element.GSh, PK]
+      }
+      else if (element.Player === 'Karim Benzema') {
+        this.benzemaData = [element.GSh, PK]
+      }
+      if (element.Player === 'Kylian Mbappé') {
+        this.mbappeData = [element.GSh, PK]
+      }
+    })
   }
 }
