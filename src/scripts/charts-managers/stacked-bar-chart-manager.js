7a6e4aff2da0acc872b3be18a11904128491621f
Dylan <batista.d09@hotmail.com>
2022-06-24 23:30:28 -0400

    omg im so bad at this. i couldnt even help u guys. im sorry. forgive me.

--- src/scripts/charts-managers/stacked-bar-chart-manager.js
+++ src/scripts/charts-managers/stacked-bar-chart-manager.js
@@ -6,11 +6,13 @@ import { AbstractChartManager } from './abstract-chart-manager'
  * @class StackedBarChartManager
  */
 export class StackedBarChartManager extends AbstractChartManager {
-  preprocess () {
-    this.shootingData = this.preprocessShootingData(this.playerHelperSingleton.groupedShootingData)
+  preprocess() {
+    this.shootingData = this.preprocessShootingData(
+      this.playerHelperSingleton.groupedShootingData
+    )
   }
 
-  initializeVariables () {
+  initializeVariables() {
     this.margin = {
       top: 50,
       right: 150,
@@ -20,7 +22,7 @@ export class StackedBarChartManager extends AbstractChartManager {
     }
   }
 
-  initializeCharts () {
+  initializeCharts() {
     this.svg = d3.select('#stacked-bar-chart-svg')
 
     // this.drawLegend()
@@ -30,61 +32,99 @@ export class StackedBarChartManager extends AbstractChartManager {
 
     console.log(this.shootingData)
 
-    this.svg
-      .attr('width', 600)
-      .attr('height', 600)
-      .attr('padding', 10)
+    this.svg.attr('width', 600).attr('height', 600).attr('padding', 10)
 
-    var subGroups = ['Mané \n Regular Shots', 'Mané \n Penalty Kicks',
-      'Benzema \n Regular Shots', 'Benzema \n Penalty Kicks',
-      'Mbappe \n Regular Shots', 'Mbappe \n Penalty Kicks']
+    var subGroups = [
+      'Mané \n Regular Shots',
+      'Mané \n Penalty Kicks',
+      'Benzema \n Regular Shots',
+      'Benzema \n Penalty Kicks',
+      'Mbappe \n Regular Shots',
+      'Mbappe \n Penalty Kicks'
+    ]
 
-    var color = d3.scaleOrdinal()
+    var color = d3
+      .scaleOrdinal()
       .domain(this.shootingData)
       .range(['#e41a1c', '#4daf4a'])
 
     var stack = d3.stack().keys(['gsh', 'gm', 'pk', 'pkm'])
-    var datasets = [d3.stack().keys(['gsh', 'gm'])(this.shootingData), d3.stack().keys(['pk', 'pkm'])(this.shootingData)]    
-    console.log('datasets', datasets)
+    /**
+     * 1 : Man RS
+     * 2 : Man PK
+     * 3 : Ben RS
+     * 4 : Ben PK
+     * 5 : Mb RS
+     * 6 : Mb PK
+     */
+    var datasets = [
+      d3.stack().keys(['Mangsh', 'Mangm'])(this.shootingData[0]),
+      d3.stack().keys(['Manpk', 'Manpkm'])(this.shootingData[0]),
+      d3.stack().keys(['Bengsh', 'Bengm'])(this.shootingData[1]),
+      d3.stack().keys(['Benpk', 'Benpkm'])(this.shootingData[1]),
+      d3.stack().keys(['Mbgsh', 'Mbgm'])(this.shootingData[2]),
+      d3.stack().keys(['Mbpk', 'Mbpkm'])(this.shootingData[2])
+    ]
 
     var num_groups = datasets.length
 
-    var xlabels = this.shootingData.map(function (d) { return d.Player })
+    var xlabels = this.shootingData.map(function (d) {
+      return d.Player
+    })
 
     // Add X axis
-    var x = d3.scaleBand()
+    var x = d3
+      .scaleBand()
       .domain(subGroups)
       .range([0, this.width])
       .padding([0.1])
-    this.svg.append('g')
+    this.svg
+      .append('g')
       .attr('transform', 'translate(' + 100 + ',' + this.height + ')')
       .call(d3.axisBottom(x).tickSizeOuter(0))
 
     // Add Y axis
     var formatPercent = d3.format('.0%')
-    var y = d3.scaleLinear()
-      .domain([0, 1])
-      .range([this.height, 0])
-    this.svg.append('g')
+    var y = d3.scaleLinear().domain([0, 1]).range([this.height, 0])
+    this.svg
+      .append('g')
       .attr('transform', 'translate(' + 100 + ',' + '0' + ')')
       .call(d3.axisLeft(y).tickFormat(formatPercent))
 
     // var stackedData = d3.stack()
     //   .keys(subGroups)(this.shootingData)
 
-    this.svg.append('g')
+    this.svg
+      .append('g')
       .selectAll('g')
       .data(datasets)
       .enter()
       .append('g')
-      .attr('fill', function (d) { return color(d.key) })
+      .attr('fill', function (d) {
+        return color(d.key)
+      })
       .selectAll('rect')
       // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function (d) { console.log('d', d); return d })
-      .enter().append('rect')
-      .attr('x', function (d) { console.log('x', x(d)); return x(d) })
-      .attr('y', function (d) { console.log('y', y(d)); return y(d) })
-      .attr('height', function (d) { console.log(y((d[0]) - y(d[1]))); return y((d[0]) - y(d[1])) })
+      .data(function (d) {
+        console.log('d', d)
+        console.log(d[0])
+        console.log(d[0][0].data.player)
+        return d
+      })
+      .enter()
+      .append('rect')
+      .attr('x', function (d) {
+        console.log('x', x(d[0].data.player))
+        return 1
+      })
+      .attr('y', function (d) {
+        console.log('y', y(d[0].data.player))
+        return 2
+      })
+      .attr('height', function (d) {
+        console.log(y(d[0] - y(d[1])))
+        return y(d[0] - y(d[1]))
+      })
       .attr('width', x.bandwidth())
   }
 
@@ -95,43 +135,82 @@ export class StackedBarChartManager extends AbstractChartManager {
    * @returns {object[]} the preprocessed data
    **/
 
-  preprocessShootingData (shootingData) {
+  // preprocessShootingData(shootingData) {
+  //   shootingData.forEach((element) => {
+  //     var PK = 0
+  //     // eslint-disable-next-line eqeqeq
+  //     if (element.PKatt != 0) {
+  //       PK = element.PK / element.PKatt
+  //     }
+  //     if (element.Player === 'Sadio Mané') {
+  //       // this.maneData = [element.Player, [element.GSh, PK]]
+  //       this.maneData = {
+  //         player: element.Player,
+  //         gsh: element.GSh,
+  //         gm: 1 - element.GSh,
+  //         pk: PK,
+  //         pkm: 1 - PK
+  //       }
+  //     } else if (element.Player === 'Karim Benzema') {
+  //       // this.benzemaData = [element.Player, [element.GSh, PK]]
+  //       this.benzemaData = {
+  //         player: element.Player,
+  //         gsh: element.GSh,
+  //         gm: 1 - element.GSh,
+  //         pk: PK,
+  //         pkm: 1 - PK
+  //       }
+  //     } else if (element.Player === 'Kylian Mbappé') {
+  //       // this.mbappeData = [element.Player, [element.GSh, PK]]
+  //       this.mbappeData = {
+  //         player: element.Player,
+  //         gsh: element.GSh,
+  //         gm: 1 - element.GSh,
+  //         pk: PK,
+  //         pkm: 1 - PK
+  //       }
+  //     }
+  //   })
+  //   return [this.maneData, this.benzemaData, this.mbappeData]
+  // }
+
+  preprocessShootingData(shootingData) {
     shootingData.forEach((element) => {
       var PK = 0
       // eslint-disable-next-line eqeqeq
       if (element.PKatt != 0) {
-        PK = element.PK / element.PKatt
+        PK = Number(element.PK) / Number(element.PKatt)
       }
       if (element.Player === 'Sadio Mané') {
         // this.maneData = [element.Player, [element.GSh, PK]]
-        this.maneData = {
+        this.maneDataG = {
           player: element.Player,
-          gsh: element.GSh,
-          gm: 1 - element.GSh,
-          pk: PK,
-          pkm: 1 - PK
+          Mangsh: Number(element.GSh),
+          Mangm: 1 - Number(element.GSh),
+          Manpk: PK,
+          Manpkm: 1 - PK
         }
       } else if (element.Player === 'Karim Benzema') {
         // this.benzemaData = [element.Player, [element.GSh, PK]]
-        this.benzemaData = {
+        this.benzemaDataG = {
           player: element.Player,
-          gsh: element.GSh,
-          gm: 1 - element.GSh,
-          pk: PK,
-          pkm: 1 - PK
+          Bengsh: Number(element.GSh),
+          Bengm: 1 - Number(element.GSh),
+          Benpk: PK,
+          Benpkm: 1 - PK
         }
       } else if (element.Player === 'Kylian Mbappé') {
         // this.mbappeData = [element.Player, [element.GSh, PK]]
-        this.mbappeData = {
+        this.mbappeDataG = {
           player: element.Player,
-          gsh: element.GSh,
-          gm: 1 - element.GSh,
-          pk: PK,
-          pkm: 1 - PK
+          Mbgsh: Number(element.GSh),
+          Mbgm: 1 - Number(element.GSh),
+          Mbpk: PK,
+          Mbpkm: 1 - PK
         }
       }
     })
-    return [this.maneData, this.benzemaData, this.mbappeData]
+    return [[this.maneDataG], [this.benzemaDataG], [this.mbappeDataG]]
   }
 
   // drawLegend() {
